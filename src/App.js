28af8172e34655b2a4c44e337e6a02671183face
swatinayak76 <swati.nayak76@gmail.com>
2021-03-10 21:23:50 -0800

    Updated routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import CourseManager from './components/course-manager';
-import Header from "./components/Header";
+import Header from './components/Header';
 import CourseEditor from './components/course-editor/course-editor';
-import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import {store,perssistor} from './reducer/store';
-import {Provider} from 'react-redux'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, perssistor } from './reducer/store';
+import { Provider } from 'react-redux';
 
 function App() {
-    return (
-        <Provider store={store}>
-      <PersistGate persistor={perssistor}> 
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={perssistor}>
         <BrowserRouter>
-            <Redirect push to="/courses/table"/>
-            <Switch>
-                <Route path="/courses" render={props => <CourseManager {...props}/>}/>
-                <Route path="/editor" exact={true} render={props => <CourseEditor {...props}/>}/>
-            </Switch>
+          <Switch>
+            <Route
+              path='/courses'
+              render={(props) => <CourseManager {...props} />}
+            />
+            <Route
+              path={`courses/:layout/edit`}
+              render={(props) => <CourseEditor {...props} />}
+            />
+            <Redirect push to='/courses/table' />
+          </Switch>
         </BrowserRouter>
-        </PersistGate>
+      </PersistGate>
     </Provider>
-    );
+  );
 }
 
 export default App;
--- src/components/course-table/course-table.js
+++ src/components/course-table/course-table.js
@@ -1,38 +1,57 @@
-import React, {useEffect} from 'react';
-import CourseRow from "./course-row";
-import {Link} from "react-router-dom";
-import CourseCard from "../course-grid/course-card";
+import React, { useEffect } from 'react';
+import CourseRow from './course-row';
+import { Link, Route, Switch, useRouteMatch } from 'react-router-dom';
+import CourseCard from '../course-grid/course-card';
+import CourseEditor from '../course-editor/course-editor';
 
 function CourseTable(props) {
-    return (
-        <div>
-            <div className="row list-header">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-8 col-md-4 col-lg-5">Title</div>
-                        <div className="d-none d-md-block col-md-2 col-lg-2">Owned By</div>
-                        <div className="d-none d-md-block col-md-4 col-lg-3">Last modified by me</div>
-                        <div className="col-4 text-center text-md-left col-sm-2 col-md-2 col-lg-2">
-                            <Link to="/courses/grid"><i className="fa fa-list mr-4"></i></Link>
-                            <Link to="/courses/table"><i className="fa fa-folder mr-4"></i></Link>
-                            <i className="fa fa-sort-alpha-down"></i>
-                        </div>
-                    </div>
-                </div>
+  const { path, url } = useRouteMatch();
+  console.log(path);
+  return (
+    <div>
+      <div className='row list-header'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-8 col-md-4 col-lg-5'>Title</div>
+            <div className='d-none d-md-block col-md-2 col-lg-2'>Owned By</div>
+            <div className='d-none d-md-block col-md-4 col-lg-3'>
+              Last modified by me
             </div>
-            <div className="row list-content">
-                <div className="container">
-                    {props.courses.map(row =>
-                        <CourseRow
-                            key={row._id}
-                            course={row}
-                            reload={props.reload}
-                            updateCourse={props.updateCourse}
-                            deleteCourse={props.deleteCourse}/>)}
-                </div>
+            <div className='col-4 text-center text-md-left col-sm-2 col-md-2 col-lg-2'>
+              <Link to='/courses/grid'>
+                <i className='fa fa-list mr-4'></i>
+              </Link>
+              <Link to='/courses/table'>
+                <i className='fa fa-folder mr-4'></i>
+              </Link>
+              <i className='fa fa-sort-alpha-down'></i>
             </div>
+          </div>
         </div>
-    );
+      </div>
+      <div className='row list-content'>
+        <Switch>
+          <Route exact path={path}>
+            <div className='container'>
+              {props.courses.map((row) => (
+                <CourseRow
+                  key={row._id}
+                  course={row}
+                  reload={props.reload}
+                  updateCourse={props.updateCourse}
+                  deleteCourse={props.deleteCourse}
+                />
+              ))}
+            </div>
+          </Route>
+          <Route
+            path={`${path}/edit/course/:courseId`}
+            render={(props) => <CourseEditor {...props} />}
+          />
+        </Switch>
+      </div>
+    </div>
+  );
 }
 
 export default CourseTable;
